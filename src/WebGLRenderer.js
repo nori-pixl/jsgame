9d9ba557cc7b740a6a7c901d8900d6d9c0c4a565
nori-pixl <sato.shigure4@gmail.com>
2026-06-18 22:55:45 +0900

    Create WebGLRenderer.js

---
+++ src/WebGLRenderer.js
@@ -0,0 +1,170 @@
+export class WebGLRenderer {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.gl = canvas.getContext('webgl2');
+    if (!this.gl) {
+      throw new Error('WebGL2 not supported');
+    }
+    this.initGL();
+    this.initShaders();
+    this.initBuffers();
+  }
+
+  initGL() {
+    const gl = this.gl;
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  }
+
+  initShaders() {
+    const gl = this.gl;
+    const vsSource = `#version 300 es
+      in vec2 aPosition;
+      in vec2 aTexCoord;
+      uniform vec2 uResolution;
+      uniform vec2 uTranslation;
+      uniform vec2 uScale;
+      uniform float uRotation;
+      out vec2 vTexCoord;
+      void main() {
+        float cosR = cos(uRotation);
+        float sinR = sin(uRotation);
+        vec2 rotatedPosition = vec2(
+          aPosition.x * cosR - aPosition.y * sinR,
+          aPosition.x * sinR + aPosition.y * cosR
+        );
+        vec2 scaledPosition = rotatedPosition * uScale;
+        vec2 worldPosition = scaledPosition + uTranslation;
+        vec2 zeroToOne = (worldPosition + vec2(10.0, 10.0)) / 20.0;
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 clipSpace = zeroToTwo - 1.0;
+        gl_Position = vec4(clipSpace, 0.0, 1.0);
+        vTexCoord = aTexCoord;
+      }`;
+
+    const fsSource = `#version 300 es
+      precision highp float;
+      in vec2 vTexCoord;
+      uniform vec4 uColor;
+      uniform sampler2D uTexture;
+      uniform bool uUseTexture;
+      out vec4 fragColor;
+      void main() {
+        if (uUseTexture) {
+          fragColor = texture(uTexture, vTexCoord);
+        } else {
+          fragColor = uColor;
+        }
+      }`;
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vsSource);
+    gl.compileShader(vertexShader);
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fsSource);
+    gl.compileShader(fragmentShader);
+
+    this.program = gl.createProgram();
+    gl.attachShader(this.program, vertexShader);
+    gl.attachShader(this.program, fragmentShader);
+    gl.linkProgram(this.program);
+
+    this.positionLocation = gl.getAttribLocation(this.program, 'aPosition');
+    this.texCoordLocation = gl.getAttribLocation(this.program, 'aTexCoord');
+    this.resolutionLocation = gl.getUniformLocation(this.program, 'uResolution');
+    this.translationLocation = gl.getUniformLocation(this.program, 'uTranslation');
+    this.scaleLocation = gl.getUniformLocation(this.program, 'uScale');
+    this.rotationLocation = gl.getUniformLocation(this.program, 'uRotation');
+    this.colorLocation = gl.getUniformLocation(this.program, 'uColor');
+    this.textureLocation = gl.getUniformLocation(this.program, 'uTexture');
+    this.useTextureLocation = gl.getUniformLocation(this.program, 'uUseTexture');
+  }
+
+  initBuffers() {
+    const gl = this.gl;
+    this.positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+    const positions = [
+      -0.5, -0.5,
+       0.5, -0.5,
+      -0.5,  0.5,
+      -0.5,  0.5,
+       0.5, -0.5,
+       0.5,  0.5,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    this.texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+    const texCoords = [
+      0.0, 1.0,
+      1.0, 1.0,
+      0.0, 0.0,
+      0.0, 0.0,
+      1.0, 1.0,
+      1.0, 0.0,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+  }
+
+  loadTexture(url, callback) {
+    const gl = this.gl;
+    const texture = gl.createTexture();
+    const image = new Image();
+    
+    // 🌟 GitHubのエンジンからローカル画像（CORS制限）を安全に読み込むための設定
+    image.crossOrigin = 'anonymous'; 
+    
+    image.onload = () => {
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      callback(texture);
+    };
+    image.src = url;
+  }
+
+  clear() {
+    const gl = this.gl;
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  }
+
+  render(sprites) {
+    const gl = this.gl;
+    gl.useProgram(this.program);
+    gl.uniform2f(this.resolutionLocation, this.canvas.width, this.canvas.height);
+
+    sprites.forEach(sprite => {
+      if (!sprite.visible || !sprite.isLoaded) return;
+
+      gl.enableVertexAttribArray(this.positionLocation);
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+      gl.vertexAttribPointer(this.positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+      gl.enableVertexAttribArray(this.texCoordLocation);
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+      gl.vertexAttribPointer(this.texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+      gl.uniform2f(this.translationLocation, sprite.physicsBody.position.x, sprite.physicsBody.position.y);
+      gl.uniform2f(this.scaleLocation, sprite.w, sprite.h);
+      gl.uniform1f(this.rotationLocation, sprite.physicsBody.angle);
+
+      if (sprite.texture) {
+        gl.uniform1i(this.useTextureLocation, 1);
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, sprite.texture);
+        gl.uniform1i(this.textureLocation, 0);
+      } else {
+        gl.uniform1i(this.useTextureLocation, 0);
+        gl.uniform4fv(this.colorLocation, sprite.color);
+      }
+
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
+    });
+  }
+}
